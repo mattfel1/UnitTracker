efb1f333c68f06820180efd08b29f6c6436dab28
mattfel1 <mattfel@stanford.edu>
2017-01-17 15:43:04 -0800

    fix

--- status.sh
+++ status.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-exit 0
+if [ ! -f results ]; then
+  echo "FAIL: No results found"
+  exit 1
+fi
+
+errors=(`cat results | grep -i "fail\|unknown" | wc -l`)
+if [[ $errors != 0 ]]; then
+  echo "FAIL: Errors found"
+  exit 1
+else
+  echo "Succes"
+  exit 0
+fi
